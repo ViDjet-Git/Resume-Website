2fead1193da3764eb75f28e92bcee8a087a45a69
Andriy Onishchuk <onishchuk221@gmail.com>
2023-07-20 09:03:42 +0300

    Adaptive position of download button

--- js/script.js
+++ js/script.js
@@ -7,3 +7,41 @@ function CopyText(that) {
 	inp.remove();
 	alert("Copied!!!");
 }
+
+function getScrollbarWidth() {
+
+  // Creating invisible container
+  const outer = document.createElement('div');
+  outer.style.visibility = 'hidden';
+  outer.style.overflow = 'scroll'; // forcing scrollbar to appear
+  outer.style.msOverflowStyle = 'scrollbar'; // needed for WinJS apps
+  document.body.appendChild(outer);
+
+  // Creating inner element and placing it in the container
+  const inner = document.createElement('div');
+  outer.appendChild(inner);
+
+  // Calculating difference between container's full width and the child width
+  const scrollbarWidth = (outer.offsetWidth - inner.offsetWidth);
+
+  // Removing temporary elements from the DOM
+  outer.parentNode.removeChild(outer);
+
+  return scrollbarWidth;
+
+}
+
+const btn = document.getElementById('download');
+var screenWidth = window.innerWidth;
+var scrollbar = getScrollbarWidth();
+console.log(screenWidth - btn.offsetWidth, screenWidth, btn.offsetWidth);
+btn.style.left = parseInt(screenWidth - btn.offsetWidth - scrollbar,10)+'px';
+
+
+addEventListener("resize", (event) => {
+	scrollbar = getScrollbarWidth();
+	screenWidth = window.innerWidth;
+	console.log(screenWidth - btn.offsetWidth, screenWidth, btn.offsetWidth);
+	btn.style.left = parseInt(screenWidth - btn.offsetWidth - scrollbar,10)+'px';
+});
+
